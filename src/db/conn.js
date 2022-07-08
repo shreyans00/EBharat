14faffe9664d9a8d59245cd64934caa2e2cbeac7
Shreyans <Shreyansraj3@gmail.com>
2022-07-08 17:13:07 +0530

    atlas

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,7 +1,12 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/stud_regis").then(() => {
+// mongoose.connect("mongodb://localhost:27017/stud_regis").then(() => {
+//     console.log(`connection success`);
+// }).catch((e) => {
+//     console.log(`no connection`);
+// })
+
+const DB = `mongodb+srv://Shreyans:shreyans@cluster0.d5cqq.mongodb.net/StudInfo?retryWrites=true&w=majority`;
+mongoose.connect(DB).then(() => {
     console.log(`connection success`);
-}).catch((e) => {
-    console.log(`no connection`);
-})
+}).catch((e) => console.log(`no connection`));
